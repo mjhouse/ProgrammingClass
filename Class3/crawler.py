cb309dafda8f72a61e68b7fc6dc9234d9bf6024c
Chris <cshenry1993@gmail.com>
2023-05-28 10:13:38 -0400

    premerge commit

--- Class3/crawler.py
+++ Class3/crawler.py
@@ -4,11 +4,22 @@ import requests
 
 STARTING_URL = "https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token"
 
+
+
+def get_page_text(url_to_download):
+    page = requests.get(url_to_download)
+    text = page.text
+    return text
+
+
 def main():
-    page = requests.get(STARTING_URL)
+    text = get_page_text(STARTING_URL)
+    print(text)
+#     page = requests.get(STARTING_URL)
     
-    print(page.text)
-    print(page.status_code)
+#     print(page.text)
+#     print(page.status_code)
+
 
 if __name__=='__main__':
     main()
