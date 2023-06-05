8ee40c0dcb247c2b33ab72d36f8f772a0de6ab3b
Michael House <mjhouse@protonmail.com>
2023-06-05 16:57:46 -0500

    Added PdfFiller class

--- Class4/script.py
+++ Class4/script.py
@@ -4,22 +4,53 @@ import requests
 
 from pypdf import PdfReader, PdfWriter
 
+class PdfFiller:
+
+    def __init__(self, path):
+        self.reader = PdfReader(path)
+        self.writer = PdfWriter()
+        self.writer.append_pages_from_reader(self.reader)
+
+    def read(self):
+        return self.reader.get_form_text_fields()
+
+    def write(self, fields):
+        for page in self.writer.pages:
+            self.writer.update_page_form_field_values(
+                page,
+                fields)
+
+    def save(self, path):
+        self.writer.write(path)
+
 def main():
-    reader = PdfReader("scratch/document.pdf")
-    fields = reader.get_form_text_fields()
+    form_filler = PdfFiller("scratch/document.pdf")
 
-    # for key, value in fields.items():
-    #     print(f"{key} = {value}")
+    fields = form_filler.read()
 
-    fields["NewSoftware-version"] = 290
+    for key, value in fields.items():
+        print(f"{key} = {value}")
 
-    writer = PdfWriter()
-    writer.append_pages_from_reader(reader)
+    fields["NewSoftware-version"] = 123
+    form_filler.write(fields)
 
-    for page in writer.pages:
-        writer.update_page_form_field_values(page,fields)
+    form_filler.save("scratch/output.pdf")
 
-    writer.write("scratch/output.pdf")
+    # reader = PdfReader("scratch/document.pdf")
+    # fields = reader.get_form_text_fields()
+
+    # # for key, value in fields.items():
+    # #     print(f"{key} = {value}")
+
+    # fields["NewSoftware-version"] = 290
+
+    # writer = PdfWriter()
+    # writer.append_pages_from_reader(reader)
+
+    # for page in writer.pages:
+    #     writer.update_page_form_field_values(page,fields)
+
+    # writer.write("scratch/output.pdf")
 
 if __name__=='__main__':
     main()
